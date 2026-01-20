19cda3f2d5a40dd17571e7056db483172ad1dea7
TheRealAditya04 <adityabansal281206@gmail.com>
2026-01-21 00:31:26 +0530

    Fix Supabase SSL connection for Vercel

--- db.js
+++ db.js
@@ -1,9 +1,5 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import pkg from "pg";
 const { Pool } = pkg;
-console.log("DB URL FROM ENV:", process.env.SUPABASE_DB_URL);
 
 export const pool = new Pool({
   connectionString: process.env.SUPABASE_DB_URL,
